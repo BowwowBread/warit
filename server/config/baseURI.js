498644e7bb6f180bc1ce78308067a3307ef31866
BowwowBread <bowwow.bread@gmail.com>
2017-10-30 17:27:09 +0900

    fix baseuri

--- server/config/baseURI.js
+++ server/config/baseURI.js
@@ -1,8 +1,9 @@
 let baseURI
-if (process.env.NODE_ENV == "production") {
-  baseURI = "ec2-13-125-41-251.ap-northeast-2.compute.amazonaws.com:3000"  
+
+if (process.env.NODE_ENV == "development") {
+  baseURI = "localhost:3000"    
 } else {
-  baseURI = "localhost:3000"  
+  baseURI = "ec2-13-125-41-251.ap-northeast-2.compute.amazonaws.com:3000"  
 }
 
 exports.baseURI = baseURI
