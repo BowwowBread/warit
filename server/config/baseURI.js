9d7c9fe47cf1beb7b0e5b29af346f392dbdbc2b1
BowwowBread <bowwow.bread@gmail.com>
2017-10-31 18:03:44 +0900

    fix

--- server/config/baseURI.js
+++ server/config/baseURI.js
@@ -1,7 +1,8 @@
 let baseURI
-var env = (process.env.NODE_ENV || 'test').trim();
-  
-if (env == "test") {
+
+
+if (process.env.NODE_ENV === "test") {
+
   baseURI = "http://localhost:3000"    
 } else {
   baseURI = "https://ec2-13-125-41-251.ap-northeast-2.compute.amazonaws.com:3000"  
